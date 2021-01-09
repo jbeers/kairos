60afff3d9bde9ba9984cff4c7b99e14606de16e4
Jacob Beers <jacobtbeers@gmail.com>
2021-01-09 00:04:26 -0600

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  siteMetadata: {
+    title: "Kairos",
+  },
+  plugins: [
+    "gatsby-plugin-postcss",
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-mdx",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
