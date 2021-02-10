43c5465abf74d095c8dbe5799e19d25870d9833e
William Griffin <willagrif@gmail.com>
2021-02-09 20:51:06 -0600

    new pages

---
+++ src/pages/ministries/Metro Mudd.js
@@ -0,0 +1,32 @@
+import * as React from "react"
+import { MinistryTile } from '../../components';
+import { MinistryLayout } from '../../layouts';
+
+
+const spark = 'https://picsum.photos/seed/picsum1/200/300';
+const fire = 'https://picsum.photos/seed/something/200/300';
+const beacon = 'https://picsum.photos/seed/picsum2/200/300';
+
+const Content = () => {
+    return <>
+		<p>This ministry serves anyone who would enjoy a beverage on Sunday morning.</p>
+		<p>We serving coffee, greet visitors, pray for others, and answer questions about the church or activities.</p>
+		<p>Each barista serves on a designated Sunday. They arrive at 8:30 am for setup and cleanup once the sermon starts.</p>
+		<p>To receive care from this ministry please speak to any of our baristas; we love meeting new people.</p>
+		<p>We love seeing everyone on Sunday mornings and being able to talk to and pray with them.</p>
+		<p>We are open to volunteers! </p>
+		<p>To become a volunteer or if you need any further information please email Sara Rosser at saraherosser@gmail.com.</p>
+    </>
+}
+// markup
+const MinistriesPage = () => {
+    return <MinistryLayout
+        name = "Spark"
+        description = "A youth ministry for kids ages 1-9"
+        content = { <Content /> }
+        mainImage = { spark }
+        galleryImages = { [ spark, fire, beacon, fire ]}
+    />
+}
+
+export default MinistriesPage
---
+++ src/pages/ministries/Prophetic.js
@@ -0,0 +1,29 @@
+import * as React from "react"
+import { MinistryTile } from '../../components';
+import { MinistryLayout } from '../../layouts';
+
+
+const spark = 'https://picsum.photos/seed/picsum1/200/300';
+const fire = 'https://picsum.photos/seed/something/200/300';
+const beacon = 'https://picsum.photos/seed/picsum2/200/300';
+
+const Content = () => {
+    return <>
+		<p>This ministry serves anyone who is looking to get some prophetic words to help them understand how God sees them.</p>
+		<p>We engage in prophetic words online and during service.</p>
+		<p>To receive care from this ministry please sign up online. </p>
+		<p>We love watching people get excited about how Jesus feels about them.</p>
+    </>
+}
+// markup
+const MinistriesPage = () => {
+    return <MinistryLayout
+        name = "Spark"
+        description = "A youth ministry for kids ages 1-9"
+        content = { <Content /> }
+        mainImage = { spark }
+        galleryImages = { [ spark, fire, beacon, fire ]}
+    />
+}
+
+export default MinistriesPage
