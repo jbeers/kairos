3ffb17991cd97ad5b1b98ddb3e79b60b33ed3a2f
Jacob Beers <jacobtbeers@gmail.com>
2024-06-18 09:01:01 -0500

    Correct some encoding issues

--- src/pages/statement-of-faith.js
+++ src/pages/statement-of-faith.js
@@ -12,7 +12,7 @@ export default () => {
             and in Jesus Christ, his only Son, our Lord,<br />
             who was conceived by the Holy Spirit,<br />
             born of the Virgin Mary,<br />
-            suffered under PonDus Pilate,<br />
+            suffered under Pontius Pilate,<br />
             was crucified, died and was buried;<br />
             he descended into hell;<br />
             on the third day he rose again from the dead;<br />
@@ -25,33 +25,33 @@ export default () => {
             the holy Church,<br />
             the communion of saints,<br />
             the forgiveness of sins,<br />
-            the resurrecDon of the body,<br />
+            the resurrection of the body,<br />
             and life everlasting.
         </p>
 
         <p>
             We believe that God has given the church the five-fold ministry of apostles, prophets, evangelists, teachers, and
-            pastors. Each of these voices bring unique value to the body of Christ. Every ChrisDan can grow in these giLings.
+            pastors. Each of these voices bring unique value to the body of Christ. Every Christian can grow in these giftings.
         </p>
 
         <ul>
-            <li>Apostle – believers sent by God to break new ground and call people into their desDny</li>
-            <li>Prophet – believers who hear the word of the Lord for the present situaDon</li>
-            <li>Evangelist – believers who have a passion to reach the lost with the good news</li>
-            <li>Teacher – believers who equip and train others in applying the scriptures in pracDcal ways</li>
-            <li>Pastor – believers who care for the body of Christ providing comfort, counseling, and support</li>
+            <li>Apostle - believers sent by God to break new ground and call people into their destiny</li>
+            <li>Prophet - believers who hear the word of the Lord for the present situation</li>
+            <li>Evangelist - believers who have a passion to reach the lost with the good news</li>
+            <li>Teacher - believers who equip and train others in applying the scriptures in practical ways</li>
+            <li>Pastor - believers who care for the body of Christ providing comfort, counseling, and support</li>
         </ul>
 
         <p>
-            We believe that the giLs of the Spirit are acDve today. The giLs of the Spirit include a word of wisdom, a word of
-            knowledge, faith, giLs of healings, working of miracles, prophecy, discerning of spirits, different kinds of tongues,
-            and interpretaDon of tongues.
+            We believe that the gifts of the Spirit are active today. The gifts of the Spirit include a word of wisdom, a word of
+            knowledge, faith, gifts of healings, working of miracles, prophecy, discerning of spirits, different kinds of tongues,
+            and interpretation of tongues.
         </p>
         <p>
             We believe that God has created man and woman in his image, so gender is important to how God has created
             each of us. We believe that marriage is between a man and woman and reveals his love. We believe that God
             intends sexuality as an expression of love within marriage. We believe that, though humans have natural desires
-            and urges, God has called us to submit these desires and urges to the truth of God’s plan.
+            and urges, God has called us to submit these desires and urges to the truth of God's plan.
         </p>
     </PageLayout>
 }
